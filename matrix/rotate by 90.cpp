7c093b06e56fdb9668c27bfe78a6f217609aa339
Ishan Kumar Srivastava <57336738+ishan0805@users.noreply.github.com>
2022-02-06 15:05:45 +0530

    rotate by 90

---
+++ matrix/rotate by 90.cpp
@@ -0,0 +1,18 @@
+void rotate(vector<vector<int> > &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = start; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+        start++;
+    }
+}
